858e63b977167014e160c30a922c981b7b57060c
DELPAS Cedric <cedric.delpas@cgxaero.com>
2018-02-16 14:35:54 +0100

    Désactivation de la constante HAVE_UNISTD_H

--- config-msvc.h
+++ config-msvc.h
@@ -143,7 +143,7 @@
 #define HAVE_SYS_TYPES_H 1
 
 /* Define to 1 if you have the <unistd.h> header file. */
-#define HAVE_UNISTD_H 1
+/*#define HAVE_UNISTD_H 1*/
 
 /* Define to 1 if you have the <zlib.h> header file. */
 #define HAVE_ZLIB_H 1
